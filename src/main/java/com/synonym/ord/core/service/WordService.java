acd2ea5a9e5e68b978c59b4a9da55d0f53671c90
Linda <linda.mollmark@gmail.com>
2017-10-08 21:52:44 +0200

    Removed unused method and variables

--- src/main/java/com/synonym/ord/core/service/WordService.java
+++ src/main/java/com/synonym/ord/core/service/WordService.java
@@ -13,44 +13,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class WordService {
 
-	private String word;
-	private String meaning;
-	private char letter;
-	private int numberOfTrials;
-	private int numberOfCorrectAnswers;
-	private Player player;
-	private int randomNumber;
-	private List<Integer> correctAnswerCount;
-	private List<Integer> numberToCountList;
-	
-	@Autowired
-	@Qualifier("getWordDAO")
-	private WordDAO wordDAO;
-	
-	public WordService() {
-		super();
-	}
-	
-	public void init(char letter, Player player) {
-		this.letter = letter;
-		this.player = player;
-		numberOfTrials = 0;
-		numberOfCorrectAnswers = 0;
-		correctAnswerCount = new ArrayList<>();
-		numberToCountList = new ArrayList<>();
-		for (int i = 0; i < 11; i++) {
-			correctAnswerCount.add(0);
-			numberToCountList.add(i);	
-		}
-	}
+    @Autowired
+    @Qualifier("getWordDAO")
+    private WordDAO wordDAO;
 
+    public WordService() {
+        super();
+    }
 
-	public List<Word> getWordsFromLetter(String letter) {
-		List<Word> words = wordDAO.getWordsFromLetter(letter);
-		return words;
-	}
+    public List<Word> getWordsFromLetter(String letter) {
+//		if (letter.equals("mix")){
+//			wordDAO.getRandomWords();
+//		}
+        List<Word> words = wordDAO.getWordsFromLetter(letter);
+        return words;
+    }
 
-	public Word getWordFromId(String id) {
-		return wordDAO.getWordFromId(Integer.parseInt(id));
-	}
+    public Word getWordFromId(String id) {
+        return wordDAO.getWordFromId(Integer.parseInt(id));
+    }
 }
